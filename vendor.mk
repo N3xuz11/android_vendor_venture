bbf318e3f5c0209000331242faf09c49614cbad5
Topazstix <robertdrone@ovi.com>
2014-09-25 20:10:13 -0600

    simple version update for exp builds

--- vendor.mk
+++ vendor.mk
@@ -3,10 +3,11 @@ export VENDOR := venture
 
 # Version info
 ROM_VERSION_MAJOR := 0
-ROM_VERSION_MINOR := 22
+ROM_VERSION_MINOR := 007
 ROM_VERSION_MAINTENANCE := 
 # B# Beta
 # A# Alpha
 # T# Test
 # R# Release
-ROM_VERSION_TAG := A1
+# EXP = Breaks and kills kittens.
+ROM_VERSION_TAG := EXP
