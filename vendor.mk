1258760d09b8c89f359c1e9bf86e8362a203f475
Vedant Mathur <atomicspaceindian@users.noreply.github.com>
2014-11-03 15:55:21 -0800

    0.25 R7

--- vendor.mk
+++ vendor.mk
@@ -6,7 +6,7 @@ ROM_VERSION_MAJOR := 0
 ROM_VERSION_MINOR := 25
 ROM_VERSION_MAINTENANCE := 
 # R Revision WILL DESTROY KITTUHNS
-ROM_VERSION_TAG := R6
+ROM_VERSION_TAG := R7
 # Increment MAJOR by 1 for every weekly release
 # Increment MINOR by 1 every time you starty working on a new aspect and/or something new is added
 # Increment TAG by 1 for every revision of code modification youa re doing for an aspect and/or feature
